bdf5bb17d97610b2f0a48b6d70417fa95580c347
Yuri Campos <yuricampos@me.com>
2013-04-04 17:02:25 -0300

    adicao de classe lebytes

--- trabalho-ori/src/Arquivo/LeBytes.java
+++ trabalho-ori/src/Arquivo/LeBytes.java
@@ -4,10 +4,25 @@
  */
 package Arquivo;
 
+import java.io.File;
+
 /**
  *
  * @author yuricampos
  */
 public class LeBytes {
+    
+    private final String DIRECTORY_ROOT = "/users/yuricampos/Documents/ori/trabalho-ori/trabalho-ori/src/corpus/";
+    File indexinvfile = new File(DIRECTORY_ROOT+"indexinvfile.dat");
+    File indice = new File(DIRECTORY_ROOT+"indice.dat");
+    File invfile = new File(DIRECTORY_ROOT+"invfile.dat");
+    
+    public void iniciaLeitura(int memoria){
+    int sizeindexinvfile = (int) indexinvfile.length();
+    int sizeindice = (int) indice.length();
+    int sizeinvfile = (int) invfile.length();
+    
+        
+    }
     
 }
