813ac6ada58068c28876e6c5c8f713bfbca1b46a
Pablo Henrique <pablohenrique@linuxmint.(none)>
2013-04-04 10:02:59 -0300

    estruturacao

--- trabalho-ori/src/Arquivo/ManipulacaoArquivos.java
+++ trabalho-ori/src/Arquivo/ManipulacaoArquivos.java
@@ -1,3 +1,5 @@
+package Arquivo;
+
 
 import java.io.BufferedReader;
 import java.io.File;
---
+++ trabalho-ori/src/Arquivo/SalvaBytes.java
@@ -0,0 +1,15 @@
+package Arquivo;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author pablohenrique
+ */
+public class SalvaBytes {
+    
+}
